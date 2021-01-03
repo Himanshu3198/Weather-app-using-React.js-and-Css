1e7710a3828f3b874c5ddf830cd1ae575d395fef
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-01-03 20:50:02 +0530

    deployment settings

---
+++ build/precache-manifest.6eb5272f51d137868c8c5458bf34bb89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c9e02063fde38d64b5422bf245fe0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff58c0c1eec5992aad60",
+    "url": "/static/css/main.98d571b7.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff58c0c1eec5992aad60",
+    "url": "/static/js/main.257fa52c.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "af411e85fc7342af648265c2c2363d75",
+    "url": "/static/media/cold-weather-boy.af411e85.jpg"
+  },
+  {
+    "revision": "afc269ad37f94b8d033350b3d9234d46",
+    "url": "/static/media/warm1.afc269ad.jpg"
+  }
+]);
